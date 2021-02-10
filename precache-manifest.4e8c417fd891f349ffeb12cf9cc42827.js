73e5373cdf2aaf6e38728c500d383a2e5e0dd745
phz2712 <lhp2712@gmail.com>
2021-02-10 09:59:34 +0700

    Updates

---
+++ precache-manifest.4e8c417fd891f349ffeb12cf9cc42827.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5ae6769f3b211daac6bb8f103262eb",
+    "url": "/page.youthdev.github.io/index.html"
+  },
+  {
+    "revision": "7ff534ddd74442dca72b",
+    "url": "/page.youthdev.github.io/static/css/2.58fd382b.chunk.css"
+  },
+  {
+    "revision": "d1e3a995e57256c0de89",
+    "url": "/page.youthdev.github.io/static/css/main.a47665bf.chunk.css"
+  },
+  {
+    "revision": "7ff534ddd74442dca72b",
+    "url": "/page.youthdev.github.io/static/js/2.25dd5258.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/page.youthdev.github.io/static/js/2.25dd5258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e3a995e57256c0de89",
+    "url": "/page.youthdev.github.io/static/js/main.065831e9.chunk.js"
+  },
+  {
+    "revision": "25808c2b3da500e52c92",
+    "url": "/page.youthdev.github.io/static/js/runtime-main.46d9e633.js"
+  },
+  {
+    "revision": "2f70c62e5734dede421916c2fd521eb8",
+    "url": "/page.youthdev.github.io/static/media/agile1.2f70c62e.jpg"
+  },
+  {
+    "revision": "1fefcff729543286cf21ae41b6012aba",
+    "url": "/page.youthdev.github.io/static/media/bigImage1.1fefcff7.jpg"
+  },
+  {
+    "revision": "85b1957a43df1834b703355167bd609f",
+    "url": "/page.youthdev.github.io/static/media/bigImage4.85b1957a.jpg"
+  },
+  {
+    "revision": "6cedd7a1e475cac7dbce5f2da7adc36f",
+    "url": "/page.youthdev.github.io/static/media/hand_stop_domino2.6cedd7a1.jpg"
+  },
+  {
+    "revision": "04f724d5751552702babb09ecbc27f6f",
+    "url": "/page.youthdev.github.io/static/media/services_ai.04f724d5.jpg"
+  },
+  {
+    "revision": "5e2eaed07c9e485109f5dd834473f99b",
+    "url": "/page.youthdev.github.io/static/media/services_automation-test.5e2eaed0.jpg"
+  },
+  {
+    "revision": "0c7f0918948771d37d7dca53db9a7eea",
+    "url": "/page.youthdev.github.io/static/media/services_ci.0c7f0918.png"
+  },
+  {
+    "revision": "e487e33f7cf12eb92420c3de64c173e0",
+    "url": "/page.youthdev.github.io/static/media/services_code-qualifying-1.e487e33f.jpg"
+  },
+  {
+    "revision": "c28a0e042585d3316fa19423a82e938a",
+    "url": "/page.youthdev.github.io/static/media/services_code-qualifying-2.c28a0e04.jpg"
+  },
+  {
+    "revision": "29601d2ac3a61dc0758024833ef98291",
+    "url": "/page.youthdev.github.io/static/media/services_control-flow.29601d2a.png"
+  },
+  {
+    "revision": "3f45c886a8cdfa3e1378784303d1ca1e",
+    "url": "/page.youthdev.github.io/static/media/services_high-availability.3f45c886.png"
+  },
+  {
+    "revision": "f326aab462e13325d3c5accd19d95c73",
+    "url": "/page.youthdev.github.io/static/media/services_monthly-report-1.f326aab4.png"
+  },
+  {
+    "revision": "94c173333f9386eb78880e90a7ac49df",
+    "url": "/page.youthdev.github.io/static/media/services_monthly-report-2.94c17333.jpg"
+  },
+  {
+    "revision": "1eb835a4249335bed01f5569fc4bb0b0",
+    "url": "/page.youthdev.github.io/static/media/services_right-role-1.1eb835a4.jpg"
+  },
+  {
+    "revision": "3a92f1a3026907455ab45604490d48d1",
+    "url": "/page.youthdev.github.io/static/media/services_right-role-2.3a92f1a3.jpg"
+  },
+  {
+    "revision": "c7eaa7301806553fcb1513ae4dc38e13",
+    "url": "/page.youthdev.github.io/static/media/services_transparent-process.c7eaa730.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/page.youthdev.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/page.youthdev.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/page.youthdev.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/page.youthdev.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
